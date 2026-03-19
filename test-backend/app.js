fd437b63928e1f9254e30ff0bcf8c62b681cd70f
Irene Chen <132230881+theirenechen12@users.noreply.github.com>
2026-03-19 16:29:49 -0700

    Create app.js

---
+++ test-backend/app.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <View>
+      {users.map(user => (
+        <Text key={user.id}>{user.name}</Text>
+      ))}
+    </View>
+  );
